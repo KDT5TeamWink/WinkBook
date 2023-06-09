e54f67820ac6a4dcef799f1ce41898d3e67252e3
saeyeon <saeyeon220@daum.net>
2023-06-10 00:01:28 +0900

    test

--- src/Components/Views/MainPage/MainPage.tsx
+++ src/Components/Views/MainPage/MainPage.tsx
@@ -1,20 +1,20 @@
-import './MainPage.scss';
-import Carousel from './Component/Carousel';
-import ajax from '@/Apis/adminAuth';
-import Main from './Component/Main';
-import Recommand from './Component/Recommand';
+import "./MainPage.scss";
+import Carousel from "./Component/Carousel";
+import ajax from "src/Apis/adminAuth";
+import Main from "./Component/Main";
+import Recommand from "./Component/Recommand";
 
 const params = new URLSearchParams(location.search);
-console.log(params.get('code'));
+console.log(params.get("code"));
 async function auth() {
   try {
-    const res = await ajax.get('/products');
+    const res = await ajax.get("/products");
     console.log(res);
   } catch (err) {
     console.log(err);
   }
 }
-if (params.get('code')) {
+if (params.get("code")) {
   auth();
 }
 
