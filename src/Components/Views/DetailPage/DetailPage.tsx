05289cc3c77d7be79572b4e1ac8368b5ccb5fba0
hahahaday12 <hahahaday12@naver.com>
2023-06-15 00:10:56 +0900

    feat: DetailPage 기능 추가
    - 장바구니 버튼 클릭시 localstorage 저장
    - 추가 스타일 수정

--- src/Components/Views/DetailPage/DetailPage.tsx
+++ src/Components/Views/DetailPage/DetailPage.tsx
@@ -1,8 +1,9 @@
 import BookCustom from '../../../bookcustom/bookcustom';
-import { useEffect, useState } from 'react';
-import './DetailPage.scss'
-import { useParams } from 'react-router-dom';
+import { useEffect, useState, useRef } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+import TopHeader from './components/TopHeader';
 import { getDetail } from '@/Apis/productApi';
+import './DetailPage.scss'
 
 function DetailPage(){
 
@@ -18,9 +19,9 @@ function DetailPage(){
     selling: string,
     description:string 
   }
-  const [detail, setDetail] = useState<DetailInfo>({} as DetailInfo);
-  const [book, setBook] = useState([]);
 
+  const navigate = useNavigate();
+  const [detail, setDetail] = useState<DetailInfo>({} as DetailInfo);
 
   const { productNo } = useParams();
   async function getDetails() {
@@ -32,35 +33,61 @@ function DetailPage(){
     }
   }
 
+  const [isScrolled, setIsScrolled] = useState(false);
+  const [ScrollY, setScrollY] = useState(0);
+
+  function handleScroll() {
+    if (ScrollY > 800) {
+      setScrollY(window.pageYOffset);
+      setIsScrolled(true);
+    } else {
+      setScrollY(window.pageYOffset);
+      setIsScrolled(false);
+    }
+  }
+  
+  useEffect(() => {
+    function scrollListener() {
+      window.addEventListener("scroll", handleScroll);
+    } //  window 에서 스크롤을 감시 시작
+    scrollListener(); // window 에서 스크롤을 감시
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    }; //  window 에서 스크롤을 감시를 종료
+  });
+    
+
   useEffect(() => {
     (async () => {
       await getDetails();
     })();
   }, []);
 
-  const BuyBook = (detailnum) => {
+  const BuyBook = (detail:any) => {
     let Cart = localStorage.getItem('cart');
+    if(Cart === null) {
+      Cart = [];
+    } else {
+      Cart = JSON.parse(Cart);
+    }
 
-    // if(Cart === null) {
-    //   Cart = [];
+    const datalist = {
+      "buy": []
+      ,"rent": []
+    };
+
+    // if (Cart.some((item) => item.id === detail.id)) {
+    //   alert('이미 장바구니에 담으셨습니다.');
     // } else {
-    //   Cart = JSON.parse(Cart);
+      Cart.push(detail);
+      Cart = new Set(Cart);
+      Cart = [...Cart];
+      localStorage.setItem('cart', JSON.stringify(Cart));
+      datalist.buy = Cart;
+      console.log(datalist);
+      alert('장바구니에 담겼습니다.');
+      navigate('/cart');
     // }
-
-    // let datalist = {
-    //   "buy": []
-    //   ,"rent": []
-    // }1
-
-
-    // datalist.buy[1]= productid;
-    
-    // buy - [1,2,4,56]
-    // rent - [1,3,6,7]
-    // let c = localStorage.getIte(cart);
-    // c.buy 
-    // Cart.push()
-    // console.log(detail);
   }
 
     const disableLinkClick = (event) => {
@@ -73,6 +100,20 @@ function DetailPage(){
         match.replace('href', 'data-disabled-href')
       )
     : '';
+
+    const renderCardPriceItems = () => {
+      const cardPriceItems = {
+        '카드/간편결제할인': '',
+        '무이자 할부': '',
+        '소득공제300원': '',
+      };
+
+      return Object.entries(cardPriceItems).map(([key]) => (
+        <span key={key}>{`${key}`}</span>
+      ));  
+    }
+
+    
   
   return(
     <>
@@ -84,8 +125,13 @@ function DetailPage(){
 
       <div className="LeftContainer">
         <div className="TitleBox">
-          <span>{detail.product_name} </span>
+          <span>{detail.product_name}</span>
+          <span>-단돈 1,000원으로 시작할수 있는</span>
+          <div className='InnerTitleBox'>
+            <p>윙크북 {detail.product_name} 전자책 출간일  2023-0505</p>
+          </div>
         </div>
+
         <div className="BookimgBox">
           <img src={detail.detail_image}/>
         </div>
@@ -104,37 +150,39 @@ function DetailPage(){
           <div className="ContentContainer">
             <span className="ContentBox">줄거리</span>
             <span className="contentText">
-            <p></p>
+            <p>{detail.summary_description}</p>
             </span>
           </div>
           <div className="CardPrice">
-            <span className="CardText">카드</span>
-            <span className="PriceNumber">무이자 할부</span>
-            <span className="PriceNumber">소득공제300원</span>
+            {renderCardPriceItems()}
           </div>
 
           <div className="ButtonContainer">
-            <button className="CartAdd" onClick={()=> BuyBook(detail.product_no)}> 책 구매하기</button>
+            <button className="CartAdd" onClick={()=> BuyBook(detail)}> 책 구매하기</button>
             <button className="BookBill">책 대여하기</button>
-
           </div>
         </div>
       </div>
+
+      <div className='BookContent'></div>
+      <span className='BookContent-text'>출판사 제공 <br/>책소개</span>
+      <div className={`TopBox ${isScrolled ? 'slide-down' : ''}`}>
+        {isScrolled ? <TopHeader productName={detail.product_name} productPrice={detail.price}/> : ""}
+      </div> 
+
       <div className="InnerContent"dangerouslySetInnerHTML={{ __html: modifiedDescription}} 
       onClick={disableLinkClick}>
       </div>
 
+      <div className='Bookple-Container'>
+        <span className='Bookple-Container__text'>북플 BOOKPLE</span>
+        <ul className='Bookple-Container__coment'>
+          <li>이책의 마니아가 남긴글</li>
+          <li>친구가 남긴글</li>
+          <li>내가 남긴글</li>
+        </ul>
+      </div>
     </div>
-    {/* <div className="InnerContent"dangerouslySetInnerHTML={{ __html: modifiedDescription}} 
-    onClick={disableLinkClick}>
-      
-      </div> */}
-    {/* <div dangerouslySetInnerHTML={{ __html: detail.detail_image }}>
-    </div> */}
-
-    {/* <div>
-      <img src={detail.detail_image}/>
-    </div> */}
     </>
   );
 }
