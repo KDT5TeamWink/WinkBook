4a48a76f3710a3169820c6a8abcf96815264210e
hahahaday12 <hahahaday12@naver.com>
2023-06-15 23:46:20 +0900

    fix: 변경사항 update

--- src/Components/Views/MainPage/Component/Genre.tsx
+++ src/Components/Views/MainPage/Component/Genre.tsx
@@ -26,13 +26,16 @@ export default function Genre({ category }: { category: Props }) {
       <div className="left">
         <div className="tag-box">
           <h1 id={category.type}>{category.type}</h1>
-          <div>tags....</div>
+          {/* <div>tags....</div> */}
         </div>
       </div>
 
       <div className="right">
+        <img className='Top-bookContainer' src="./public/images/Group 8.png"/>
         {list &&
           list.map((item) => (
+            <>
+            {/* <img className='Bottom-bookContainer' src="./public/images/Group 8.png"/> */}
             <BookInfo
               key={item.product_no}
               productNo={item.product_no}
@@ -42,7 +45,20 @@ export default function Genre({ category }: { category: Props }) {
               price={item.price}
               summary={item.summary_description}
             />
+            {/* <div className='bottombook'>
+              <img src="./public/images/Group 8.png"/>
+            </div> */}
+            {/* {Array.from({ length: 1 }).map((_, index) => (
+              <div className='bottomImage-container'>
+              <img
+                key={index}
+                src="./public/images/Group 8.png"
+              />
+              </div>
+            ))} */}
+            </>
           ))}
+          <img className='Bottom-bookContainer' src="./public/images/Group 8.png"/>
       </div>
     </div>
   );
